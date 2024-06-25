e0e7ad85f7919894e75bc47134fead2685bd1e25
Irshad <irshad89434@gmail.com>
2024-06-25 19:30:10 +0530

    currying with closure

--- currying.js
+++ currying.js
@@ -1,6 +1,18 @@
-function mul(x,y){
-    return x*y;
+// function mul(x,y){
+//     return x*y;
+// }
+
+// const mulByTwo = mul.bind(this, 2);
+// console.log(mulByTwo(5));
+
+
+function mul(x){
+    return function(y){
+        return x*y;
+    }
 }
 
-const mulByTwo = mul.bind(this, 2);
-console.log(mulByTwo(5))
+const mulByTwo = mul(2);
+const mulByFive = mul(5)
+console.log(mulByTwo(10))
+console.log(mulByFive(10))
